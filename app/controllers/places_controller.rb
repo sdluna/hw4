439dab8653d63c72b00ea9f03517cbb28c67ba7b
Simone D'Luna <simone.dluna@kellogg.northwestern.edu>
2024-03-09 16:54:24 +0000

    Authorization changes - allowing only users to see their entries

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,12 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"]})
+    if session["user_id"] != nil
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"]})
+    else 
+     flash["notice"] = "Sorry, you must be logged in to see your entries"
+     redirect_to "/login"
+    end
   end
 
   def new
